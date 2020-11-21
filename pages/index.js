6712a88891a04056c8e091d7a8690a99459bf817
Andra Malmen <andra.malmen@tieto.com>
2020-11-21 22:03:24 +0200

    Add initial coming soon

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Andra's Blog</title>
+            </Head>
+
+            <main className={styles.main}>
+                <h1 className={styles.title}>Coming soon-ishhhhh</h1>
+            </main>
+        </div>
+    );
+}
